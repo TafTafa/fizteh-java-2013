963e0018d9e7a8431804cc419c10bafee3248fb0
TafTafa <TafTafy@gmail.com>
2013-11-04 23:27:23 +0400

    Task 03 - FileMap

---
+++ src/ru/fizteh/fivt/students/kocurba/filemap/command/PutCommand.java
@@ -0,0 +1,36 @@
+package ru.fizteh.fivt.students.kocurba.filemap.command;
+
+import java.io.IOException;
+
+import ru.fizteh.fivt.students.kocurba.shell.ShellState;
+import ru.fizteh.fivt.students.kocurba.shell.command.Command;
+
+public class PutCommand implements Command<State> {
+
+	@Override
+	public int getArgCount() {
+		return 2;
+	}
+
+	@Override
+	public String getCommandName() {
+		return "put";
+	}
+
+	@Override
+	public void executeCommand(ShellState<State> state, String[] arguments)
+			throws IOException {
+		if (state.getState().getCurrentTable() == null) {
+			System.err.println("no table");
+			return;
+		}
+		String oldValue = state.getState().getCurrentTable()
+				.put(arguments[1], arguments[2]);
+		if (oldValue == null) {
+			System.out.println("new");
+		} else {
+			System.out.println("overwrite");
+			System.out.println(oldValue);
+		}
+	}
+}
---
+++ src/ru/fizteh/fivt/students/kocurba/filemap/command/RemoveCommand.java
@@ -0,0 +1,34 @@
+package ru.fizteh.fivt.students.kocurba.filemap.command;
+
+import java.io.IOException;
+
+import ru.fizteh.fivt.students.kocurba.shell.ShellState;
+import ru.fizteh.fivt.students.kocurba.shell.command.Command;
+
+public class RemoveCommand implements Command<State> {
+
+	@Override
+	public int getArgCount() {
+		return 1;
+	}
+
+	@Override
+	public String getCommandName() {
+		return "remove";
+	}
+
+	@Override
+	public void executeCommand(ShellState<State> state, String[] arguments)
+			throws IOException {
+		if (state.getState().getCurrentTable() == null) {
+			System.err.println("no table");
+			return;
+		}
+		if (state.getState().getCurrentTable().remove(arguments[1]) != null) {
+			System.out.println("removed");
+		} else {
+			System.out.println("not found");
+		}
+	}
+
+}
