6f8e2062c41f2860652e8b91971a1f4628b616d5
TafTafa <TafTafy@gmail.com>
2013-11-04 23:29:38 +0400

    Task 04 - MultiFileHashMap

---
+++ src/ru/fizteh/fivt/students/kocurba/multifilehashmap/command/CreateCommand.java
@@ -0,0 +1,33 @@
+package ru.fizteh.fivt.students.kocurba.multifilehashmap.command;
+
+import java.io.IOException;
+
+import ru.fizteh.fivt.storage.strings.Table;
+import ru.fizteh.fivt.students.kocurba.filemap.command.State;
+import ru.fizteh.fivt.students.kocurba.shell.ShellState;
+import ru.fizteh.fivt.students.kocurba.shell.command.Command;
+
+public class CreateCommand implements Command<State> {
+
+	@Override
+	public int getArgCount() {
+		return 1;
+	}
+
+	@Override
+	public String getCommandName() {
+		return "create";
+	}
+
+	@Override
+	public void executeCommand(ShellState<State> state, String[] arguments)
+			throws IOException {
+		Table newTable = state.getState().getTableProvider()
+				.createTable(arguments[1]);
+		if (newTable == null) {
+			System.err.println(arguments[1] + " exists");
+			return;
+		}
+		System.out.println("created");
+	}
+}
