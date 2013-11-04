677d3e16e56cd5b40f24580c5411c5e91e3e9a55
TafTafa <TafTafy@gmail.com>
2013-11-04 23:30:33 +0400

    Task 05 - JUnit

---
+++ src/ru/fizteh/fivt/students/kocurba/storage/strings/FileTableProviderFactory.java
@@ -0,0 +1,16 @@
+package ru.fizteh.fivt.students.kocurba.storage.strings;
+
+import ru.fizteh.fivt.storage.strings.TableProvider;
+import ru.fizteh.fivt.storage.strings.TableProviderFactory;
+
+public class FileTableProviderFactory implements TableProviderFactory {
+
+	@Override
+	public TableProvider create(String dir) {
+		if (dir == null) {
+			throw new IllegalArgumentException();
+		}
+		return new FileTableProvider(dir);
+	}
+
+}
---
+++ src/ru/fizteh/fivt/students/kocurba/storage/strings/test/FileTableProviderFactoryTest.java
@@ -0,0 +1,41 @@
+package ru.fizteh.fivt.students.kocurba.storage.strings.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ru.fizteh.fivt.storage.strings.TableProviderFactory;
+import ru.fizteh.fivt.students.kocurba.storage.strings.FileTableProviderFactory;
+
+/**
+ * Task 05 - JUnit
+ * 
+ * JUnit tests for {@link FileTableProviderFactory} class
+ * 
+ * @author Alina Kocurba
+ * 
+ */
+public class FileTableProviderFactoryTest {
+
+	private TableProviderFactory factory;
+
+	@Before
+	public void initFactory() {
+		factory = new FileTableProviderFactory();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullDirectoryName() {
+		factory.create(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNonExistingDirectoryName() {
+		factory.create("/non-existing");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testIncorrectSymbolsInDirectoryName() {
+		factory.create("/\\");
+	}
+
+}
