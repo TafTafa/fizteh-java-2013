07b7c5ea4da5cf7fb854dfc8fc40899ccee68498
TafTafa <TafTafy@gmail.com>
2013-12-23 01:36:11 +0400

    fix2

---
+++ src/ru/fizteh/fivt/students/kocurba/JUnit/Main.java
@@ -0,0 +1,45 @@
+package ru.fizteh.fivt.students.kocurba.JUnit;
+
+import ru.fizteh.fivt.storage.strings.TableProviderFactory;
+import ru.fizteh.fivt.students.kocurba.filemap.command.*;
+import ru.fizteh.fivt.students.kocurba.multifilehashmap.command.CreateCommand;
+import ru.fizteh.fivt.students.kocurba.multifilehashmap.command.DropCommand;
+import ru.fizteh.fivt.students.kocurba.multifilehashmap.command.UseCommand;
+import ru.fizteh.fivt.students.kocurba.shell.Shell;
+import ru.fizteh.fivt.students.kocurba.shell.StateWrap;
+import ru.fizteh.fivt.students.kocurba.shell.command.Command;
+import ru.fizteh.fivt.students.kocurba.storage.strings.FileTableProviderFactory;
+
+public class Main {
+
+    public static void main(String[] args)  {
+
+        TableProviderFactory tableProviderFactory = new FileTableProviderFactory();
+        StateWrap<State> state = new StateWrap<State>(new State(tableProviderFactory.create(System
+                .getProperty("fizteh.db.dir")), null));
+        Shell<State> shell = new Shell<State>(state);
+
+
+        Command<State> putCommand = new PutCommand();
+        Command<State> getCommand = new GetCommand();
+        Command<State> removeCommand = new RemoveCommand();
+        Command<State> exitCommand = new ExitCommand();
+        Command<State> createCommand = new CreateCommand();
+        Command<State> dropCommand = new DropCommand();
+        Command<State> newUseCommand = new NewUseCommand();
+        Command<State> commitCommand = new CommitCommand();
+        Command<State> rollbackCommand = new RollbackCommand();
+
+        Command[] commands = {putCommand, getCommand, removeCommand, createCommand, dropCommand, newUseCommand,
+                exitCommand, commitCommand, rollbackCommand};
+
+        if (0 != args.length) {
+            shell.batchMode(args, commands);
+        } else {
+            shell.interactiveMode(commands);
+        }
+
+        System.exit(0);
+    }
+
+}
---
+++ src/ru/fizteh/fivt/students/kocurba/JUnit/NewUseCommand.java
@@ -0,0 +1,39 @@
+package ru.fizteh.fivt.students.kocurba.JUnit;
+
+import ru.fizteh.fivt.storage.strings.Table;
+import ru.fizteh.fivt.students.kocurba.filemap.command.State;
+import ru.fizteh.fivt.students.kocurba.shell.StateWrap;
+import ru.fizteh.fivt.students.kocurba.shell.command.Command;
+
+import java.io.IOException;
+
+public class NewUseCommand implements Command<State> {
+
+    @Override
+    public int getArgCount() {
+        return 1;
+    }
+
+    @Override
+    public String getCommandName() {
+        return "use";
+    }
+
+    @Override
+    public void executeCommand(StateWrap<State> state, String[] arguments)
+            throws IOException {
+        if (state.getState().getCurrentTable() != null && state.getState().getCurrentTable().size() > 0) {
+            System.err.println(state.getState().getCurrentTable().size() + " unsaved changes");
+            //state.getState().getCurrentTable().commit();
+            return;
+        }
+
+        Table newTable = state.getState().getTableProvider().getTable(arguments[1]);
+        if (newTable == null) {
+            System.err.println(arguments[1] + " not exists");
+            return;
+        }
+        state.getState().setCurrentTable(newTable);
+        System.out.println("using " + arguments[1]);
+    }
+}
