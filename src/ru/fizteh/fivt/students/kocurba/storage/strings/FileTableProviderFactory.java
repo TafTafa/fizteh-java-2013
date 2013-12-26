2563af4a293baea804c8238f90ae01b5208c4c45
TafTafa <TafTafy@gmail.com>
2013-12-26 06:12:55 +0400

    fix-JU

--- src/ru/fizteh/fivt/students/kocurba/storage/strings/FileTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/kocurba/storage/strings/FileTableProviderFactory.java
@@ -8,8 +8,8 @@ public class FileTableProviderFactory implements TableProviderFactory {
     @Override
     public TableProvider create(String dir) {
         if (dir == null) {
-           // throw new IllegalArgumentException();
-            System.exit(1);
+            throw new IllegalArgumentException();
+            //System.exit(1);
         }
         return new FileTableProvider(dir);
     }
