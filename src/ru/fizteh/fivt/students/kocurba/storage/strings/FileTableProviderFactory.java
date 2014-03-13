5eb5d729ef1de14fe4ce18e45650252f4710f73f
TafTafa <TafTafy@gmail.com>
2014-03-13 23:34:53 +0400

    ju_fix

--- src/ru/fizteh/fivt/students/kocurba/storage/strings/FileTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/kocurba/storage/strings/FileTableProviderFactory.java
@@ -2,14 +2,23 @@ package ru.fizteh.fivt.students.kocurba.storage.strings;
 
 import ru.fizteh.fivt.storage.strings.TableProvider;
 import ru.fizteh.fivt.storage.strings.TableProviderFactory;
+import java.io.File;
 
 public class FileTableProviderFactory implements TableProviderFactory {
 
     @Override
     public TableProvider create(String dir) {
-        if (dir == null) {
+        if ((dir == null) || dir.trim().equals("")) {
             throw new IllegalArgumentException();
-            //System.exit(1);
+        }
+        File directoryFile = new File(dir);
+        if (!directoryFile.exists()) {
+            if (!directoryFile.mkdir()) {
+                throw new IllegalArgumentException();
+            }
+        }
+        if (!directoryFile.isDirectory()) {
+            throw new IllegalArgumentException();
         }
         return new FileTableProvider(dir);
     }
